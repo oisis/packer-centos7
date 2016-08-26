45f4a2c5a807c22096b508ccfc41e90d07f26a82
OiSiS <oisis@o2.pl>
2016-08-26 02:02:02 +0200

    Add Puppet yum repo

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,6 +4,7 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl grub2-tools net-tools
 
 yum -y install epel-release.noarch
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum -y install puppet
 
 # Change network interfaces names back to ethX
