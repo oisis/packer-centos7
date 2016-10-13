faaadf3e416d83aff430b33d02e7c2da8273c918
OiSiS <oisis@o2.pl>
2016-10-14 00:48:01 +0200

    Fix for network interface names

--- scripts/base.sh
+++ scripts/base.sh
@@ -7,9 +7,3 @@ yum -y install epel-release.noarch
 rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
 yum -y install puppet
 gem install hiera-eyaml hiera-eyaml-kms
-
-# Change network interfaces names back to ethX
-sed -i 's/rhgb/net.ifnames=0 rhgb/g' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
-echo 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="08:00:27:de:0e:0e", ATTR{type}=="1", KERNEL=="eth*", NAME="eth0"' > /etc/udev/rules.d/70-persistent-net.rules
-
